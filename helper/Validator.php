6ae871f674b7d771214b16e48299237b9899e750
yasmeen <yasmeen144@yahoo.com>
2025-03-02 04:39:45 +0200

    last

--- helper/Validator.php
+++ helper/Validator.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'session.php';
 class Validator
 {
     private $errors = [];
@@ -28,46 +28,48 @@ class Validator
     private function validate_require($field, $value)
     {
         if (empty($value)) {
-            $this->errors[$field][] = "The $field is required.";
+            Session::set('errors', $field . "is required");
         }
     }
 
     private function validate_email($field, $value)
     {
         if (!filter_var($value, FILTER_VALIDATE_EMAIL)) {
-            $this->errors[$field][] = "The $field must be a valid email.";
+            Session::set('errors', $field . "invalid");
+            
         }
     }
 
     private function validate_numeric($field, $value)
     {
         if (!is_numeric($value)) {
-            $this->errors[$field][] = "The $field must be a number.";
+            Session::set('errors', $field . "must be a number");
+            
         }
     }
 
     private function validate_min_length($field, $value, $min_length)
     {
         if (strlen($value) < $min_length) {
-            $this->errors[$field][] = "The $field must be at least $min_length characters.";
+            Session::set('errors' ,'sorry' . $field . 'must be at least' . $min_length . 'characters.');
         }
     }
 
     private function validate_max_length($field, $value, $max_length)
     {
         if (strlen($value) > $max_length) {
-            $this->errors[$field][] = "The $field must not exceed $max_length characters.";
+            Session::set('errors' ,'sorry' . $field . 'must not exceed ' . $max_length . 'characters.');
+
         }
     }
 
     public function hasErrors()
     {
-        return !empty($this->errors);
+        return Session::has('errors');
     }
 
     public function getErrors()
     {
-        return $this->errors;
+        return Session::getAll('errors');
     }
 }
-?>
